b672bbbed4d7a75fec4410fd6df59805d4080002
Naveen R M <naveen.rm.551@gmail.com>
2024-04-23 23:34:31 -0400

    Atlee | Changes

--- atlee.py
+++ atlee.py
@@ -26,6 +26,8 @@ from langchain_core.prompts import PromptTemplate
 from langchain.retrievers.multi_vector import MultiVectorRetriever, SearchType
 from langchain.storage import InMemoryStore
 from langchain_anthropic import ChatAnthropic
+from langchain_ai21 import ChatAI21
+from langchain_community.chat_models import ChatLiteLLM
 
 from tqdm.autonotebook import tqdm
 
@@ -41,6 +43,8 @@ import base64
 import uuid
 import io
 
+import asyncio
+
 import time
 import os
 
@@ -48,6 +52,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
+
 
 
 with open('auth.yaml') as file:
@@ -80,20 +85,24 @@ interests = ''
 PINECONE_INDEX_NAME = os.environ['PINECONE_INDEX_NAME']
 PINECONE_API_KEY=os.environ['PINECONE_API_KEY']
 ANTHROPIC_API_KEY=os.environ['ANTHROPIC_API_KEY']
+AI21_API_KEY=os.environ["AI21_API_KEY"]
+OPENAI_API_KEY=os.environ['OPENAI_API_KEY']
 # ANTHROPIC_API_KEY='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'
 LLAMA_API_KEY='LL-w5KOiBu1f11QJb7xYGk0iQ32LXkpt8pEdJmLrraHEDG6hg4h7E0XB5Kc5TygtEdJ'
 
-EMBEDDING_MODEL='all-MiniLM-L6-v2'
+EMBEDDING_MODEL="sentence-transformers/all-mpnet-base-v2"
 LLAMA_MODEL = "meta-llama/Llama-2-7b-chat-hf"
-CHAT_MODEL='claude-3-opus-20240229'
-FILE_NAME='sample.pdf'
+CHAT_MODEL='claude-3-haiku-20240307'
 IMAGE_MEDIA_TYPE='image/jpeg'
 
 anthropic_client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
-# chat_model = ChatAnthropic(temperature=0.6, api_key=ANTHROPIC_API_KEY, model=CHAT_MODEL, default_headers={"anthropic-beta": "tools-2024-04-04"},)
-chat_model = Ollama(base_url="http://localhost:11434", model="llama3", temperature=0.7)
+chat_model = ChatAnthropic(temperature=0.6, api_key=ANTHROPIC_API_KEY, model=CHAT_MODEL, default_headers={"anthropic-beta": "tools-2024-04-04"},)
+# chat_model = Ollama(base_url="http://localhost:11434", model="llama3", temperature=0.7)
+# chat_model = ChatAI21(model="j2-ultra", temperature = 0.7, max_tokens=8190)
+# chat_model = ChatLiteLLM(model="gpt-3.5-turbo", temperature=0.7)
 embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
 pc = Pinecone(api_key=PINECONE_API_KEY)
+processed_document = []
 
 def get_one_line_interest_from_history(search_titles):
     prompt_text="""
@@ -136,7 +145,7 @@ def create_pinecone_index(index_name):
     if index_name not in existing_indexes:
         pc.create_index(
             index_name,
-            dimension=384,
+            dimension=768,
             metric='cosine',
             spec=ServerlessSpec(
                 cloud="aws",
@@ -149,7 +158,6 @@ def create_pinecone_index(index_name):
     index = pc.Index(index_name)
     time.sleep(1)
     index.describe_index_stats()
-    return index
 
 def get_text_elements(categorized_elements):
     text_elements = [e for e in categorized_elements if e.type == "text"]
@@ -177,7 +185,7 @@ def fetch_local_image(image_path):
 
 def get_image_summaries_from_anthropic(image_media_type, local_image):
   message = anthropic_client.messages.create(
-      model="claude-3-opus-20240229",
+      model="claude-3-haiku-20240307",
       max_tokens=1024,
       messages=[
           {
@@ -203,15 +211,15 @@ def get_image_summaries_from_anthropic(image_media_type, local_image):
 
 def get_image_summaries():
   image_summaries = []
-#   image_path = os.getcwd() + '\\figures'
-#   for images in os.listdir(image_path):
-#     if not os.path.isdir(images):
-#       try:
-#         local_image = fetch_local_image(image_path + images)
-#         image_summary = get_image_summaries_from_anthropic(IMAGE_MEDIA_TYPE, local_image)
-#         image_summaries.append(image_summary.content[0].text)
-#       except FileNotFoundError as e:
-#         print(f"Error: {e}")
+  image_path = os.getcwd() + '\\figures\\'
+  for images in os.listdir(image_path):
+    if not os.path.isdir(images):
+      try:
+        local_image = fetch_local_image(image_path + images)
+        image_summary = get_image_summaries_from_anthropic(IMAGE_MEDIA_TYPE, local_image)
+        image_summaries.append(image_summary.content[0].text)
+      except FileNotFoundError as e:
+        print(f"Error: {e}")
   return image_summaries
 
 def get_overall_summary(raw_data):
@@ -221,12 +229,12 @@ def get_overall_summary(raw_data):
   """
   prompt = ChatPromptTemplate.from_template(prompt_text)
   summary_chain = (
-      {"data": lambda x : x}
+      {"data": RunnablePassthrough()}
       | prompt
       | chat_model
       | StrOutputParser()
   )
-  return summary_chain.batch(raw_data)
+  return [summary_chain.invoke(raw_data)]
 
 def store_pdf_in_pinecone(file_name, index_name, embeddings):
     raw_pdf_elements = partition_pdf(
@@ -274,6 +282,8 @@ def store_pdf_in_pinecone(file_name, index_name, embeddings):
         Document(page_content=s, metadata={id_key: img_ids[i]})
         for i, s in enumerate(img_ids)
     ]
+    
+    processed_document = text_docs + table_docs + img_summary_docs
 
     vector_store = PineconeVectorStore(embedding=embeddings, index_name=index_name)
     store = InMemoryStore()
@@ -329,7 +339,7 @@ def get_personalized_output(question, retriever, chat_model, user_personas, user
     Explain to the below user,
     {interests}
     
-    Give personalized answer to the above person.
+    Give personalized answer to the above person. Don't mention the interests of the user explicitly in your response.
     """
     
     prompt = PromptTemplate(
@@ -359,15 +369,14 @@ def get_personalized_output(question, retriever, chat_model, user_personas, user
             "context": retriever,
             "question": question,
         },
-        return_only_outputs=True
+        return_only_outputs=True,
     )
-    return result["text"]
+    return result['text']
 
 
-if __name__ == "__main__":
-
+def main():
     authenticator.login()
-    pinecone_index = create_pinecone_index(PINECONE_INDEX_NAME)
+    create_pinecone_index(PINECONE_INDEX_NAME)
 
     if st.session_state["authentication_status"]:
         if "name" in st.session_state:
@@ -383,38 +392,40 @@ if __name__ == "__main__":
 
                 st.success("File saved successfully!")
                 
-            if st.session_state:
-                if hasattr(st.session_state, 'messages'):
-                    st.session_state.messages.append({"role": "ai", "content": "Bye"})
-            
-            if prompt := st.chat_input("Let me know what's in your Mind"):
-                st.session_state.messages.append({"role": "user", "content": prompt})
-
-
-            if uploaded_file and prompt:
-                query = prompt
-                # df = pd.read_csv('out.csv')
-                # interests = get_interests(df)
-                # user_personas[user_id]["web_interests"] = interests
-                # retriever = store_pdf_in_pinecone(FILE_NAME, PINECONE_INDEX_NAME, embeddings)
-                # retriever.search_type = SearchType.mmr
-                # response = retriever.vectorstore.similarity_search(query)
-                # response = [response[i].page_content for i in range(len(response))]
-                # result = get_personalized_output(query, response, chat_model, user_personas, user_id)
-                result = "Hello"
-                st.session_state.messages.append({"role": "ai", "content": result})
-                print("Session State", end='\n\n')
-                print(st.session_state)
-
-            
             if "messages" not in st.session_state:
                 st.session_state.messages = []
-
+                st.session_state.response = []
+                st.session_state.counter = 0
+            
             for message in st.session_state.messages:
                 with st.chat_message(message["role"]):
                     st.markdown(message["content"])
+            
+            if prompt := st.chat_input("Let me know what's in your Mind", disabled=not uploaded_file):
+                with st.chat_message("user"):
+                    st.markdown(prompt)
+                st.session_state.messages.append({"role": "user", "content": prompt})
 
-            # logout()
+            if uploaded_file and prompt:
+                query = prompt
+                print(st.session_state.counter)
+                if st.session_state.counter == 0:
+                    retriever = store_pdf_in_pinecone(FILE_NAME, PINECONE_INDEX_NAME, embeddings)
+                    retriever.search_type = SearchType.mmr
+                    response = retriever.vectorstore.similarity_search(query)
+                    response = [response[i].page_content for i in range(len(response))]
+                    st.session_state.response = response
+                df = pd.read_csv('out.csv')
+                interests = get_interests(df)
+                user_personas[user_id]["web_interests"] = interests
+                recent_response = st.session_state.response
+                result = get_personalized_output(query, recent_response, chat_model, user_personas, user_id)
+                with st.chat_message("ai"):
+                    st.markdown(result)
+                st.session_state.messages.append({"role": "ai", "content": result})
+                st.session_state.counter += 1
+                print(st.session_state)
+                            
         else:
             st.sidebar.write("Please log in to view the content.")
             st.sidebar.title("Atlee Personalized AI")
@@ -430,3 +441,7 @@ if __name__ == "__main__":
                 st.error(e)
     elif st.session_state["authentication_status"] is None:
         st.warning('Please enter your username and password')
+
+
+if __name__ == "__main__":
+    main()
